bcefed695617f4896eb90d473204ea5600b4be33
MertNasipoglu <100800067+mertnasip@users.noreply.github.com>
2022-07-08 21:40:53 +0300

    Add files via upload

---
+++ wwhdb/src/Entities/Filiale.java
@@ -0,0 +1,63 @@
+package Entities;
+
+import java.util.ArrayList;
+
+public class Filiale {
+    String filialeId;
+    String filialeName;
+    String adress;
+    ArrayList<Personal> personal = new ArrayList<>();
+    ArrayList<Produkt> produkten = new ArrayList<>();
+    ArrayList<Kunde> kunden = new ArrayList<>();
+    ArrayList<Verkaufaufzeichnung> verkaeufe = new ArrayList<>();
+
+    //getters and setters
+    public String getFilialeId() {
+        return filialeId;
+    }
+    public void setFilialeId(String filialeId) {
+        this.filialeId = filialeId;
+    }
+
+    public String getFilialeName() {
+        return filialeName;
+    }
+    public void setFilialeName(String filialeName) {
+        this.filialeName = filialeName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public ArrayList<Personal> getPersonal() {
+        return personal;
+    }
+    public void setPersonal(ArrayList<Personal> personal) {
+        this.personal = personal;
+    }
+
+    public ArrayList<Produkt> getProdukten() {
+        return produkten;
+    }
+    public void setProdukten(ArrayList<Produkt> produkten) {
+        this.produkten = produkten;
+    }
+
+    public ArrayList<Kunde> getKunden() {
+        return kunden;
+    }
+    public void setKunden(ArrayList<Kunde> kunden) {
+        this.kunden = kunden;
+    }
+
+    public ArrayList<Verkaufaufzeichnung> getVerkaeufe() {
+        return verkaeufe;
+    }
+    public void setVerkaeufe(ArrayList<Verkaufaufzeichnung> verkaeufe) {
+        this.verkaeufe = verkaeufe;
+    }
+}
